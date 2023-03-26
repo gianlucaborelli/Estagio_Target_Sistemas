0b05c4ee07477e8b3511542c05d90d2fe4387d84
Gianluca Borelli <gianlucaborelli2@hotmail.com>
2023-03-26 19:11:35 -0300

    Exercicio 2 Completo

---
+++ Exercicio_2/Fibonacci.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_2
+{
+    public class Fibonacci
+    {
+        public static bool IsFibonacci(int n)
+        {
+            int a = 0;
+            int b = 1;
+
+            while (b < n)
+            {
+                int temp = a + b;
+                a = b;
+                b = temp;
+            }
+
+            return b == n;
+        }
+    }
+}
--- Exercicio_2/Program.cs
+++ Exercicio_2/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Exercicio_2;
+
+bool Finaliza = false;
+
+Console.WriteLine("Este projeto verifica se o valor inteiro digitado pertence a seguencia de Fibonacci.");
+
+do
+{
+    Console.WriteLine("Qual o valor que gostaria de verificar se pertence a Sequencia de Fibonacci?");
+
+    int valor;
+    try
+    {
+        valor = int.Parse(Console.ReadLine());
+    }
+    catch (FormatException)
+    {
+        Console.WriteLine("Entrada inválida! Digite um número inteiro.");
+        continue;
+    }
+
+    if (Fibonacci.IsFibonacci(valor))
+    {
+        Console.WriteLine("O número escolhido PERTENCE à sequência de Fibonacci.");
+    }
+    else
+    {
+        Console.WriteLine("O número escolhido NÃO PERTENCE à sequência de Fibonacci.");
+    }
+
+    Console.WriteLine("Deseja verificar um novo número?\nTecle ENTER para repetir:");
+
+    ConsoleKeyInfo keyInfo = Console.ReadKey();
+    if (keyInfo.Key != ConsoleKey.Enter)
+    {
+        Finaliza = false;
+    }
+
+    Console.Clear();
+
+} while(Finaliza==false);
+
